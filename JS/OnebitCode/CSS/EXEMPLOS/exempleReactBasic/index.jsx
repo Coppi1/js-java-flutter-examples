97f7e77efff30c1804d1aa15df12eba40efaef0c
Eduardo Coppi <educoppi00@gmail.com>
2024-03-09 18:43:16 -0300

    React_Init

---
+++ JS/OnebitCode/CSS/EXEMPLOS/exempleReactBasic/index.jsx
@@ -0,0 +1,23 @@
+const element = <h1>Olá mundo!</h1>;
+
+const anotherElement = (
+  <div>
+    <h1>Texto</h1>
+    <ul>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+    </ul>
+    <h2></h2>
+  </div>
+);
+
+function BotaoPrimario() {
+  return <button>Meu Botão</button>;
+}
+
+function Conteiner() {
+  return <div className=""></div>;
+}
---
+++ JS/OnebitCode/CSS/EXEMPLOS/viteProject/teste/src/App.jsx
@@ -0,0 +1,15 @@
+import StsText from "./components/StsText";
+import { Subtitle } from "./components/Subtitle";
+import Title from "./components/Title";
+
+export default function App() {
+  //const tech = "React";
+
+  return (
+    <div>
+      <Title></Title>
+      <Subtitle></Subtitle>
+      <StsText></StsText>
+    </div>
+  );
+}
